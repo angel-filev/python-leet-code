222eeae4388e165d48a77d35c5ea770af8a63268
K23900 <anguel.filev@finra.org>
2023-03-15 13:58:16 -0400

    Initial Commit

--- tests/linked_list_tests/sort_list_tests.py
+++ tests/linked_list_tests/sort_list_tests.py
@@ -5,27 +5,31 @@ from src.linked_lists.sort_list import SortList
 from src.models.list_node import ListNode
 
 
-class TestSolution(unittest.TestCase):
+class SortListTests(unittest.TestCase):
     def test_example_1(self):
         compare_list = CompareList()
 
         # Test case 1: Empty lists
-        self.assertTrue(compare_list.compare_lists(None, None))
+        # self.assertTrue(compare_list.compare_lists(None, None))
 
         head = ListNode(4, ListNode(2, ListNode(1, ListNode(3))))
         expected_output = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
-        self.assertTrue(compare_list.compare_lists(head, expected_output))
+        sortList: SortList = SortList()
+        self.assertTrue(compare_list.compare_lists(sortList.sort(head), expected_output))
+
     def test_example_2(self):
         head = ListNode(-1, ListNode(5, ListNode(3, ListNode(4, ListNode(0)))))
         expected_output = ListNode(-1, ListNode(0, ListNode(3, ListNode(4, ListNode(5)))))
         sortList = SortList()
-        self.assertEqual(sortList.sort(head), expected_output)
+        compare_list = CompareList()
+        self.assertTrue(compare_list.compare_lists(sortList.sort(head), expected_output))
 
     def test_example_3(self):
         head = None
         expected_output = None
         sortList = SortList()
-        self.assertEqual(sortList.sort(head), expected_output)
+        compare_list = CompareList()
+        self.assertTrue(compare_list.compare_lists(sortList.sort(head), expected_output))
 
 
 if __name__ == '__main__':
